cffd502dd28d2b457a894079067442150fea8fa7
hoang le <huyhoang7192@gmail.com>
2022-03-24 17:26:36 +0700

    fix feature 1 24/03/2022 17:25

--- src/test/java/features/user/login.feature
+++ src/test/java/features/user/login.feature
@@ -7,23 +7,20 @@ Feature: Login with user account
 
   Scenario: Login with empty data
     When click Login button
-    Then email error message is displayed
-      | Please enter your email |
+    Then email error message "Please enter your email" is displayed
 
   Scenario: Login with invalid email
-    When enter value to Email text box
-      | abc#gmail.com |
+    When enter value invalid "abc#gmail.com" to Email text box
     And click Login button
-    Then email error message is displayed
-      | Wrong email |
+    Then email error message "Wrong email" is displayed
 
     Scenario: Login with registered email and valid password
       When enter registered email to Email text box
       And enter valid password to Password text box
         | wayne123 |
       And click Login button
-      Then a web browser is on the Customer Info Page
-        | My account - Customer info |
+      Then a web browser is on the Customer Info Page with title "My account - Customer info"
+
 
 
 
--- src/test/java/stepDefinitions/user/UserLoginPageSteps.java
+++ src/test/java/stepDefinitions/user/UserLoginPageSteps.java
@@ -31,21 +31,20 @@ public class UserLoginPageSteps {
         userCustomerInfoPage = PageGeneratorManager.getUserCustomerInfoPage(driver);
     }
 
-    @When("enter value to Email text box")
+    @When("enter value invalid {string} to Email text box")
     public void enterValueToEmailTextBox(DataTable dataTable) {
         List<List<String>> email = dataTable.cells();
         userLoginPage.enterToTextBoxByName(driver, "Email", email.get(0).get(0));
     }
 
-    @Then("email error message is displayed")
-    public void emailErrorMessageIsDisplayed(DataTable dataTable) {
-        List<List<String>> errorMessage = dataTable.cells();
-        Assert.assertEquals(errorMessage.get(0).get(0), userLoginPage.getEmailErrorMessage());
+    @Then("email error message {string} is displayed")
+    public void emailErrorMessageIsDisplayed(String errorMessage) {
+        Assert.assertEquals(errorMessage, userLoginPage.getEmailErrorMessage());
     }
 
     @When("enter registered email to Email text box")
     public void enterRegisteredEmailToEmailTextBox() {
-        userLoginPage.enterToTextBoxByName(driver, "Email", UserRegisterPageSteps.email);
+        userLoginPage.enterToTextBoxByName(driver, "Email", UserCustomerInfoPageSteps.emailMain);
     }
 
     @And("enter valid password to Password text box")
@@ -54,9 +53,8 @@ public class UserLoginPageSteps {
         userLoginPage.enterToTextBoxByName(driver, "Password", password.get(0).get(0));
     }
 
-    @Then("a web browser is on the Customer Info Page")
-    public void getCustomerInfoPageTitle(DataTable dataTable) {
-        List<List<String>> title = dataTable.cells();
-        Assert.assertEquals(title.get(0).get(0), userLoginPage.getCustomerInfoPageTitle());
+    @Then("a web browser is on the Customer Info Page with title {string}")
+    public void getCustomerInfoPageTitle(String titlePage) {
+        Assert.assertEquals(titlePage, userLoginPage.getCustomerInfoPageTitle());
     }
 }
